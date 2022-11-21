e6a970e94a0f112d0810607da85ac9ac78920f7e
pingAvisek <aghimire074@gmail.com>
2022-11-21 20:13:30 +0545

    initial build || setopati script ✅

---
+++ myScript.js
@@ -0,0 +1,26 @@
+alert("Hiiiii");
+console.log("Hiiiii");
+let blockads = [
+  ".sticky-footer-bigyaapan",
+  ".mobile-fixed-ad",
+  ".full-bigyaapan",
+  ".bigyaapan-item",
+  ".mobile-bigyaapan-only",
+  ".fixed-tops",
+  ".top-bigyaapan",
+  ".desktop-bigyaapan-only",
+  ".top-bigyaapan",
+  ".mobile-bigyaapan-only",
+  ".top-full",
+  ".main-bigyaapan",
+  ".mobile-bottom-bigyaapan-only",
+  ".modalbox",
+  ".mobile-jacket-bigyaapan",
+];
+for (const i of blockads) {
+  let block = document.querySelectorAll(`${i}`);
+  block.forEach((e) => {
+    e.setAttribute("style", "display: none !important");
+  });
+}
+console.log("AdBlocker is working");
