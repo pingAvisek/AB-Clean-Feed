5266a3595154cf09df5683f2cf0d7693f3d90479
pingAvisek <aghimire074@gmail.com>
2022-11-22 21:44:21 +0545

    Online Khabar Script Added ✅

--- myScript.js
+++ myScript.js
@@ -1,4 +1,5 @@
-let blockads = [
+// For setopati.com
+let setoPatiAdDiv = [
   ".sticky-footer-bigyaapan",
   ".mobile-fixed-ad",
   ".full-bigyaapan",
@@ -15,10 +16,38 @@ let blockads = [
   ".modalbox",
   ".mobile-jacket-bigyaapan",
 ];
-for (const i of blockads) {
+for (const i of setoPatiAdDiv) {
   let block = document.querySelectorAll(`${i}`);
   block.forEach((e) => {
     e.setAttribute("style", "display: none !important");
   });
 }
-console.log("AdBlocker is working");
+console.log(
+  "%c AB Clean Feed is Working Fine! ",
+  "background: #222; color: #bada55"
+);
+
+// For onlinekhabar.com
+let onlineKhabarAdDiv = [
+  "ok-full-widht-adv",
+  "ok-sidebar-ad",
+  "okam-ad-position-wrap",
+  "home-aftersamachar-half1",
+  "okam-device-desktop",
+  "home-samachar-right2",
+  "okam-each-ad",
+  "ok-el-head-adv-square",
+];
+
+for (const i of onlineKhabarAdDiv) {
+  let block = document.querySelectorAll(`.${i}`);
+  block.forEach((e) => {
+    e.setAttribute("style", "display: none !important");
+  });
+}
+
+let headerAd = document
+  .querySelector("section") // done this as I can't find any class or id for this
+  .setAttribute("style", "display: none !important");
+
+// Still needs to work on this (connect IPS logo)
